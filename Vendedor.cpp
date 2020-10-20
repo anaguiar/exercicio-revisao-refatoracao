f69d113e240ec2ba42e926dc8ddb27efc53d0e7d
anaguiar <43219279+anaguiar@users.noreply.github.com>
2020-10-20 15:19:53 -0300

    Update Vendedor.cpp
    Construtor usando herança e polimorfismo;
    setNewVendedor;
    overload do operador <<;
    Retirada de magic numbers;
    quotaTotalAnual setada no construtor

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,35 @@
 #include <string>
 #include "Empregado.hpp"
+#define MESES 12
+class Vendedor : public Empregado
+{
 
-class Vendedor : public Empregado {
+public:
+	int quotaVendas;
 
-  public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
-	
+	Vendedor(std::string nome, int salarioHora, int quotaMensal, float horasTrabalhadas) : Empregado(salarioHora, nome, horasTrabalhadas)
+	{
+		quotaVendas = quotaTotalAnual(quotaMensal);
+	};
+
+	void setNewVendedor(string nome, int salarioHora, int quotaMensal, float horasTrabalhadas)
+	{
+		this->nome = nome;
+		this->salarioHora = salarioHora;
+		this->quotaVendas = this->quotaTotalAnual(quotaMensal);
+		this->salarioMes = this->pagamentoMes(horasTrabalhadas);
+	};
+
+	double quotaTotalAnual(int quotaMensal)
+	{
+		return quotaMensal * MESES;
+	};
+
+friend ostream &operator<<(ostream &out, const Vendedor &p)
+  {
+    out << "Nome: " << p.nome << endl;
+    out << "Salario Mes: " << p.salarioMes << endl;
+    out << "Quota Vendas: " << p.quotaVendas << endl;
+    return out;
+  };
 };
-
