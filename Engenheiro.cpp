e06794b0220592e96cc19dfb2093c4674b3b9eda
anaguiar <43219279+anaguiar@users.noreply.github.com>
2020-10-20 15:16:54 -0300

    Update Engenheiro.cpp
    Construtor do engenheiro usando herança e polimorfismo;
    Overload do operador <<
    setNewEngenheiro

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,26 @@
 #include <string>
 #include "Empregado.hpp"
 
-class Engenheiro : public Empregado {
+class Engenheiro : public Empregado
+{
 
-  public:
-    std::string nome;  
-	int projetos;
-	
+public:
+  Engenheiro(std::string nome, int salarioHora, int projetos, float horasTrabalhadas) : Empregado(salarioHora, nome, horasTrabalhadas), projetos(projetos) {};
+  int projetos;
+
+  void setNewEngenheiro(string nome, int salarioHora, int projetos, float horasTrabalhadas)
+  {
+    this->nome = nome;
+    this->salarioHora = salarioHora;
+    this->projetos = projetos;
+    this->salarioMes = this->pagamentoMes(horasTrabalhadas);
+  };
+
+  friend ostream &operator<<(ostream &out, const Engenheiro &p)
+  {
+    out << "Nome: " << p.nome << endl;
+    out << "Salario Mes: " << p.salarioMes << endl;
+    out << "Projetos: " << p.projetos << endl;
+    return out;
+  };
 };
-
