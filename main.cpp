6f92dc24843a09a79f5b2ce80cb844383ffe9152
anaguiar <43219279+anaguiar@users.noreply.github.com>
2020-10-20 15:24:57 -0300

    Update main.cpp
    Usando polimorfismo, herança, sobrecarga do operador ostream e as demais funções set,consegui não só simplificar e explicitar mais o código como gastar menos memória a criação de engenheiros e vendedores. 
    Antes 3 de cada subclasse estavam sendo alocados na memória o que foi reduzido para somente 1 apontador de cada.(Que foi deletado ao fim do processo para liberar memória).
    Esse único apontador da subclasse criada para Vendedor e Engenheiro, respectivamente aux1 e aux2, tem os atributos alterados de acordo com os passados previamente.
    Além disso o uso de magic numbers foi evitado utilizando o #define.

--- main.cpp
+++ main.cpp
@@ -1,64 +1,66 @@
 #include "Engenheiro.cpp"
 #include "Vendedor.cpp"
 
-int main() {
+#define N 3
 
-  Engenheiro eng1;
-  eng1.nome = "Joao Snow";
-  eng1.salarioHora = 35;
-  eng1.projetos = 3; 
-  std::cout << "Nome: " << eng1.nome << std::endl;
-  std::cout << "Salario Mes: " << eng1.pagamentoMes(9.5) << std::endl;
-  std::cout << "Projetos: " << eng1.projetos << std::endl;
-  std::cout << std::endl;
-  
-  Engenheiro eng2;
-  eng2.nome = "Daniela Targaryen";
-  eng2.salarioHora = 30;
-  eng2.projetos = 1; 
-  std::cout << "Nome: " << eng2.nome << std::endl;
-  std::cout << "Salario Mes: " << eng2.pagamentoMes(8) << std::endl;
-  std::cout << "Projetos: " << eng2.projetos << std::endl;  
-  std::cout << std::endl;
-  
-  Engenheiro eng3;
-  eng3.nome = "Bruno Stark";
-  eng3.salarioHora = 30;
-  eng3.projetos = 2; 
-  std::cout << "Nome: " << eng3.nome << std::endl;
-  std::cout << "Salario Mes: " << eng3.pagamentoMes(8) << std::endl;
-  std::cout << "Projetos: " << eng3.projetos << std::endl;  
-  std::cout << std::endl;
-  
-  
-  Vendedor vend1;
-  vend1.nome = "Tonho Lannister";
-  vend1.salarioHora = 20;
-  vend1.quotaMensalVendas = 5000;
-  
-  std::cout << "Nome: " << vend1.nome << std::endl;
-  std::cout << "Salario Mes: " << vend1.pagamentoMes(6) << std::endl;  
-  std::cout << "Quota vendas: " << vend1.quotaTotalAnual() << std::endl;
-  std::cout << std::endl;
-  
-  Vendedor vend2;
-  vend2.nome = "Jose Mormont";
-  vend2.salarioHora = 25;
-  vend2.quotaMensalVendas = 3000;
-  
-  std::cout << "Nome: " << vend2.nome << std::endl;
-  std::cout << "Salario Mes: " << vend2.pagamentoMes(8) << std::endl;  
-  std::cout << "Quota vendas: " << vend2.quotaTotalAnual() << std::endl; 
-  std::cout << std::endl;  
-	
-  Vendedor vend3;
-  vend3.nome = "Sonia Stark";
-  vend3.salarioHora = 30;
-  vend3.quotaMensalVendas = 4000;
-  
-  std::cout << "Nome: " << vend3.nome << std::endl;
-  std::cout << "Salario Mes: " << vend3.pagamentoMes(8) << std::endl;  
-  std::cout << "Quota vendas: " << vend3.quotaTotalAnual() << std::endl;  
-  
-  return 0;	
+float horasTrabalhadasE[N] = {9.5, 8, 8};
+
+string nomesEngenheiros[N] = {"Joao Snow",
+                              "Daniela Targaryen",
+                              "Bruno Stark"};
+
+int salarioHoraEng[N] = {35,
+                         30,
+                         30};
+int projetos[N] = {3,
+                   1,
+                   2};
+
+string nomesV[N] = {"Tonho Lannister",
+                    "Jose Mormont",
+                    "Sonia Stark"};
+
+int salarioHoraV[N] = {20,
+                       25,
+                       30};
+
+int quotaMensal[N] = {5000, 3000, 4000};
+
+int horasTrabalhadasV[N] = {6, 8, 8};
+
+int main()
+{  
+  int i;
+  Engenheiro *aux1;
+  aux1 = new Engenheiro(nomesEngenheiros[0], salarioHoraEng[0], projetos[0], horasTrabalhadasE[0]);
+  for (i = 1; i <= N; i++)
+  {
+    cout << *aux1 << endl;
+    if (i < N)
+    {
+      aux1->setNewEngenheiro(nomesEngenheiros[i], salarioHoraEng[i], projetos[i], horasTrabalhadasE[i]);
+    }
+    else
+    {
+      delete aux1;
+    }
+  };
+
+  Vendedor *aux2;
+  aux2 = new Vendedor(nomesV[0], salarioHoraV[0], quotaMensal[0], horasTrabalhadasV[0]);
+  for (i = 1; i <= N; i++)
+  {
+    if (i < N)
+    {
+      cout << *aux2 << endl;
+      aux2->setNewVendedor(nomesV[i], salarioHoraV[i], quotaMensal[i], horasTrabalhadasV[i]);
+    }
+    else
+    {
+      cout << *aux2;
+      delete aux2;
+    }
+  };
+
+  return 0;
 }
