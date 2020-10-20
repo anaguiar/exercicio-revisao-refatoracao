9391557776c204b0d8875b54cb6b4472e4d0efde
anaguiar <43219279+anaguiar@users.noreply.github.com>
2020-10-20 15:15:12 -0300

    Update Empregado.hpp
    Criação do construtor da classe Empregado e retirada de magic numbers

--- Empregado.hpp
+++ Empregado.hpp
@@ -2,26 +2,36 @@
 #define EMPREGADO_H
 
 #include <iostream>
+using namespace std;
 #include <string>
+
+#define EXTRA 8
+#define METADE 0.5
 
 class Empregado {
 	
   public:
+    string nome;
     double salarioHora;  
-    double quotaMensalVendas;  
+    double salarioMes;
 
+    Empregado(int salarioHora, string nome, float horasTrabalhadas) : salarioHora(salarioHora), nome(nome){
+      salarioMes = this->pagamentoMes(horasTrabalhadas);
+    };
 
     double pagamentoMes(double horasTrabalhadas) {
  
       double t = horasTrabalhadas;
 	  
 	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
+      if (horasTrabalhadas > EXTRA) {
+        double x = horasTrabalhadas - EXTRA;
+        t += x * METADE;
       }
 	  return t * salarioHora;
-    }
+    };
+
+    friend ostream & operator << (ostream &out, const Empregado &c);
 	
 };
 
